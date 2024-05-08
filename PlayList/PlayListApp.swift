18a90e6b2c4f0e98a1e9ae960813d46f5fe72c99
Lucas Gabriel <lucasgabrieltais16@gmail.com>
2024-05-09 07:13:24 +0800

    Initial Commit

---
+++ PlayList/PlayListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayListApp.swift
+//  PlayList
+//
+//  Created by user256729 on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
